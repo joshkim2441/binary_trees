ca2be4307ad9fe68c0d6452dffcc7d16a94afa1e
joshkim2441 <joshkim2441@gmail.com>
2024-02-28 07:14:52 +0300

    BST Remove

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,12 +12,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t nodes = 0;
 
 	if (!tree)
+	{
 		return (0);
-	if (!tree->left || !tree->right)
-		nodes = 1;
-
-	nodes = binary_tree_nodes(tree->left);
-	nodes = binary_tree_nodes(tree->right);
-
-	return (nodes);
+	}
+	else
+	{
+		nodes +	((tree->left || tree->right) ? 1 : 0);
+		nodes = binary_tree_nodes(tree->left);
+		nodes = binary_tree_nodes(tree->right);
+		return (nodes);
+	}
 }
