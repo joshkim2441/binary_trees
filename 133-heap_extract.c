c124af742eed9aa6ef2263fa0b72594812e8de24
joshkim2441 <joshkim2441@gmail.com>
2024-02-29 15:15:19 +0300

    Heap extract

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,51 +1,74 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
-/**
- * find_last_node - finds the last node in the heap
- * @root: a pointer to the root node of the tree
- * Return: the last node
-*/
-heap_t *find_last_node(heap_t *root)
+size_t t_height(const heap_t *tree)
 {
-	heap_t *queue[100];
-	int rear = -1, front = 0;
+	size_t left_ht = 0, right_ht = 0;
 
-	queue[++rear] = root;
-
-	while (front <= rear)
-	{
-		heap_t *curr = queue[front++];
-
-		if (curr->left)
-			queue[++rear] = curr->left;
-		if (curr->right)
-			queue[++rear] = curr->right;
-	}
-	return (queue[rear]);
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left_ht = 1 + t_height(tree->left);
+	if (tree->right)
+		right_ht = 1 + t_height(tree->right);
+	if (left_ht > right_ht)
+		return (left_ht);
+	return (right_ht);
 }
 
-/**
- * sift_down - sifts down a node in the heap
- * @node: a pointer to the node to be sifted down
- * Return: the sifted down node
-*/
-void sift_down(heap_t *node)
+size_t t_hsize(const binary_tree_t *tree)
 {
-	heap_t *largest;
+	size_t left_ht = 0, right_ht = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left_ht = 1 + t_hsize(tree->left);
+	if (tree->right)
+		right_ht = 1 + t_hsize(tree->right);
+	return (left_ht + right_ht);
+}
+
+void preord_trv(heap_t *root, heap_t **lnode, size_t ht)
+{
+	if (!root)
+		return;
+	if (!ht)
+		*lnode = root;
+	ht--;
+
+	preord_trv(root->left, lnode, ht);
+	preord_trv(root->right, lnode, ht);
+}
+
+void bry_heapify(heap_t *tree)
+{
+	heap_t *t1, *t2;
+	int val;
+
+	if (!tree)
+		return;
+	t1 = tree;
 
 	while (1)
 	{
-		largest = node;
-		if (node->left && node->left->n > largest->n)
-			largest = node->left;
-		if (node->right && node->right->n > largest->n)
-			largest = node->right;
-
-		if (largest == node)
+		if (!t1->left)
 			break;
-
-		swap(node, largest);
-		node = largest;
+		if (!t1->right)
+			t2 = t1->left;
+		else
+		{
+			if (t1->left->n > t1->right->n)
+				t2 = t1->left;
+			else
+				t2 = t1->right;
+		}
+		if (t1->n > t2->n)
+			break;
+		val = t1->n;
+		t1->n = t2->n;
+		t2->n = val;
+		t1 = t2;
 	}
 }
 
@@ -56,30 +79,29 @@ void sift_down(heap_t *node)
 */
 int heap_extract(heap_t **root)
 {
-	heap_t *last_node;
+	heap_t *mx_heap, *last_node;
 	int root_value;
 
 	if (!root || !*root)
 		return (0);
-	last_node = find_last_node(*root);
-	root_value = (*root)->n;
+	mx_heap = *root;
+	root_value = mx_heap->n;
 
-	if (last_node == *root)
+	if (!mx_heap->left && !mx_heap->right)
 	{
-		free(*root);
 		*root = NULL;
+		free(mx_heap);
+		return (root_value);
 	}
+	preord_trv(mx_heap, &last_node, t_height(mx_heap));
+
+	mx_heap->n = last_node->n;
+	if (last_node->parent->right)
+		last_node->parent->right = NULL;
 	else
-	{
-		if (last_node->parent->left == last_node)
-			last_node->parent->left = NULL;
-		else
-			last_node->parent->right = NULL;
-
-		(*root)->n = last_node->n;
-		free(last_node);
-
-		sift_down(*root);
-	}
+		last_node->parent->left = NULL;
+	free(last_node);
+	bry_heapify(mx_heap);
+	*root = mx_heap;
 	return (root_value);
 }
